c50ebe343e0fe005948aecb62259aaae74fca3d8
Simon <simon-gitpub@think.io>
2016-05-06 12:18:56 +0100

    test

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,11 @@
 node default {
-  hiera_include('classes')  
+  hiera_include('classes')
+  include "clamscan"
+ 
+  
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+    
 }
